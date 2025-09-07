b5e106bb91e5bcf29e65ab7ab13a277e02b61d70
Niko Petrov <rsnanse@yandex.ru>
2025-09-07 10:58:50 -0700

    Домашнее задание Управление потоком

---
+++ 5-languages/app.js
@@ -0,0 +1,20 @@
+const personLanguage = "language";
+
+switch (true) {
+  case personLanguage === "RU":
+    console.log("Привет!");
+
+    break;
+  case personLanguage === "SP":
+    console.log("Hola!");
+
+    break;
+  case personLanguage === "DE":
+    console.log("Guten tag!");
+
+    break;
+  default:
+    console.log("Hello");
+
+    break;
+}
