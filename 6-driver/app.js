6caa46c31fe2ddf67c9060e4ef600d6b4da90a88
Niko Petrov <rsnanse@yandex.ru>
2025-09-08 01:04:36 -0700

    Исправлено

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -1,12 +1,6 @@
 const user = "Nicko";
-const hasLicense = true;
+const hasLicence = true;
 const age = 18;
 const isDrunk = false;
 
-const canDrive = age >= 18 && hasLicense && !isDrunk;
-
-console.log(
-  `Водитель ${user} ${
-    canDrive ? "может водить автомобиль" : "не может водить автомобиль"
-  }`
-);
+const canDrive = age > 18 && hasLicence && !isDrunk ? "Может" : "Не может";
