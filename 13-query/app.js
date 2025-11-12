6765583f0abf09e20ed15021e52cb6c5a45464b4
Niko Petrov <rsnanse@yandex.ru>
2025-11-12 19:56:45 +0400

    Исправлено

--- 13-query/app.js
+++ 13-query/app.js
@@ -4,15 +4,9 @@ const queryObj = {
 };
 
 function createQuerryToString(object) {
-  let names = Object.keys(object);
-  let value = [];
-  for (keys in object) {
-    value.push(object[keys]);
-  }
-
-  console.log(names);
-  console.log(value);
-  return names[0] + "=" + value[0] + "&" + names[1] + "=" + value[1];
+  return Object.entries(object)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
 }
 
 console.log(createQuerryToString(queryObj));
